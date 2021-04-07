62948b979181d61c7ddabebfe8aa100e354e321b
SuyashMuley <suyashmuley5@gmail.com>
2021-04-08 00:00:47 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import jsPDF from 'jspdf'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+   
+    constructor(props) {
+        super(props)
+    };
+
+    generatePdf = () => {
+      
+      var doc = new jsPDF('p', 'pt');
+      
+      doc.text(20, 20, 'This is the first page title.')
+
+      doc.setFont('helvetica')
+      doc.setFontType('normal')
+      doc.text(20, 60, 'This is the content area.')
+      doc.addPage() // this code creates new page in pdf document
+      doc.setFont('helvetica')
+      doc.setFontType('normal')
+      doc.text(20, 100, 'This is the second page.')      
+
+      
+      doc.save('sample-file.pdf')
+    }   
+    
+   render() {
+      return (
+         <div>
+            <button onClick={this.generatePdf} type="primary">Download PDF</button> 
+         </div>
+      );
+   }
 }
 
 export default App;
